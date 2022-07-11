e8ae95964e27595e4d1dd0c18d2fb30867fb3a91
Roberto Nóbrega <r_t_4@hotmail.com>
2022-07-11 14:42:39 -0300

    Atividade_Prática_Funções_JavaScript

---
+++ Atividade_Pratica_Funcoes_JavaScript/exercicio_1.js
@@ -0,0 +1,43 @@
+/*
+            Atividade 1 (De Stephany Nush, da DIO): Alunos Aprovados
+    1. Crie uma função que recebe o array alunos e um número que vai representar a média final;
+    2. Percorra o array e popule um novo array auxiliar, apenas com os alunos, cujas notas são maiores ou
+    iguais à média final;
+    3. Utilize a técnica "object destructuring" para manipular as propriedades desejadas de cada aluno.
+*/
+
+const alunos = [
+    {
+        nome: 'João',
+        nota: 5,
+        turma: '1B',
+    },
+    {
+        nome: 'Sofia',
+        nota: 9,
+        turma: '1B',
+    },
+    {
+        nome: 'Paulo',
+        nota: 6,
+        turma: '2C',
+    },
+    {
+        nome: 'Miguel',
+        nota: 3,
+        turma: '2C',
+    },
+];
+
+function alunosAprovados(arr, media){
+    let aprovados = []; // Array Auxiliar. No momento, está vazio.
+    for(let i = 0; i < arr.length; i++){
+        const {nota, nome} = arr[i];  // Usando Object Destructuring.
+        if(nota >= media){
+            aprovados.push(nome);
+        }
+    }
+    return aprovados;
+}
+
+console.log(alunosAprovados(alunos, 5));
---
+++ Atividade_Pratica_Funcoes_JavaScript/exercicio_2.js
@@ -0,0 +1,30 @@
+/*
+                Atividade 2 (De Stephany Nush, da DIO): This
+        Dada a função calcularIdade, use os métodos call e apply para mudar o valor de this.
+         OBS: Criar próprios objetos para a atividade.
+*/
+
+function calcularIdade(anos){
+    return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
+}
+
+const pesso1 = {
+    nome: 'Maria',
+    idade: 30,
+};
+
+const pessoa2 = {
+    nome: 'Carla',
+    idade: 26,
+};
+
+const animal = {
+    nome: 'Fiona',
+    idade: 5,
+    raca: 'Pug',
+};
+
+console.log(calcularIdade.call(pessoa2, 30));
+console.log(calcularIdade.call(animal, 7));
+
+console.log(calcularIdade.apply(animal, [4])); // Similar ao call, porém o valor tem que ser passado entre [ ].
